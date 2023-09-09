cf70bafa5fee2872e10db3bf027e39f671e40d42
NafiAsib <nafi.asib@gmail.com>
2023-09-09 04:41:22 +0000

    best-time-to-buy-and-sell-stock

---
+++ best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,18 @@
+func maxProfit(prices []int) int {
+	if len(prices) < 2 {
+		return 0
+	}
+
+	profit := 0
+	minPrice := prices[0]
+
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		} else if prices[i]-minPrice > profit {
+			profit = prices[i] - minPrice
+		}
+	}
+
+	return profit
+}
